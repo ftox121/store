97fd5f4e5559d8c4b5450471049478e1dcff3d59
timerlan <lstaeee@mail.ru>
2024-08-09 09:36:06 +0300

    Add cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -8,6 +8,16 @@ from shop.models import Product
 
 class Cart:
 
+    def __len__(self):
+        """
+        Посчитать все товарные позиции
+        :return:
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
     def add(self, product, quantity=1, override_quantity=False):
         """Добавить товар"""
         product_id = str(product.id)
--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+
+from cart.cart import Cart
+from cart.forms import CartAddProduct
+from shop.models import Product
+
 
 # Create your views here.
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProduct(require_POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity = cd['quantity'], override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
